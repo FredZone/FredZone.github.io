8a7a5c3e815aa9f42a73e2aa515a448e4d25c00a
FredZone <57700838+FredZone@users.noreply.github.com>
2019-11-20 06:42:58 -0600

    New data file...

---
+++ Fossils/Data/B.js
@@ -0,0 +1,8 @@
+//GLOBAL VARIABLES=====================================================================================================
+var J =0;
+var PICcount=7;//pictures in gallery
+var NAME='Fred and the Fossils';//band name
+var WEBSITE="fredzone.github.io/Fossils";
+var EMAIL ="fkaparich@gmail.com";
+var CALL="(254)214-9980"
+var NAR ="Just a retired guy with a guitar...|Some Electronic Gear and Spare Time...|Playing Music at Care facilities in Waco|'Cause I love it!!";
