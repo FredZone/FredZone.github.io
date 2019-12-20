66315e5bb8e3cd9e7a705463d3ea054c905e816d
FredZone <57700838+FredZone@users.noreply.github.com>
2019-12-20 08:42:01 -0600

    general Sync with worked out simplification

--- Lyric/Top/GlobalRoutines.js
+++ Lyric/Top/GlobalRoutines.js
@@ -43,6 +43,63 @@ var WINDwt;
 
 document.getElementById('fileInput').addEventListener('change', readSingleFile, false);
 
+
+//from commonLyric=======================================
+function longestLine() {
+    var count = 0;
+    var i = 0;
+    while (i < ARRlines.length) {
+        var ltype = lineType(ARRlines[i]);
+        if (ltype == 'lyric' | ltype == 'chord') {
+            if (ARRlines[i].length > count) {
+                count = ARRlines[i].length;
+            }
+        }
+        i++;
+    }
+    return count;
+}
+
+function createSetSelector() //B
+{ //alert ('Lyric Common function');//creates an option box for the file SetList.txt in the top directory
+    //statusMsg("Starting Javascript: Loading List of Sets....");not all programs use a status message
+    var path = "SetList.txt";
+    var request = new XMLHttpRequest();
+    request.open("GET", path, false);
+    request.send(null);
+    var content = request.responseText;
+    var SETS = content.split("\n");
+    ihtml = "SELECT A PLAYLIST: <select id='Set' style='font-size:2vw;font-family:Courrier New;' onchange='selectSet(this.value)'><optgroup>\n<option selected>ALL TUNES</option>";
+    j = 0;
+    while (j < SETS.length) {
+        if (SETS[j] !== "ALL TUNES") {
+            ihtml = ihtml + "\n<option>" + SETS[j] + "</option>";
+        }
+        j = j + 1;
+    }
+    ihtml = ihtml + "\n</optgroup></Select>";
+    document.getElementById("setSelect").innerHTML = ihtml;
+    //selectSet("ALL TUNES");//xxx0 was unnecessary
+}
+
+function lineType(str){   
+    var ans ="lyric";
+    if(str.substring (0,7).toUpperCase()=="IREALB:"){ans = "irealb";}    
+    else if(str.substring (0,4).toUpperCase()=="TAB:"){ans = "tab";}    
+    else if(str.substring (0,4)=="http")  {ans = "link";}
+    else if(str.substring (0,3)=="@@@")  {ans = "noteTech";} 
+    else if(str.substring (0,2)=="@@")  {ans = "noteTriv";}
+    else if(str.substring (0,1)=="@")  {ans = "note";}
+    else if(str.substring (0,1)=="#") {ans = "header";}
+    else if(str.substring (0,2)=="$$") {ans = "noteLive";}
+    else if(str.substring (0,1)=="$") {ans = "spacer";}
+    else if(str.search(":")>-1) {ans = "hash";}
+    else if(str.indexOf("|") >-1){ans ="chord";}
+    return ans;}
+
+//from commonLyric=======================================
+
+
 function XXXXX(){alert(TEMP);}//use this function as 'process' to test your code: fileGetLocal('XXXXX')
 
 function fileGetLocal(process){TEMPflag=process;document.getElementById('fileInput').click();}
